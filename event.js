aecdc3eb0da593b32f1bdeba482a94e409a200d1
rajlevi <rajlevix11@gmail.com>
2025-03-11 14:54:44 +0100

    Javascript fixed it

---
+++ event.js
@@ -0,0 +1,17 @@
+function validate() {
+    let value_name = document.getElementById("input_name").value;
+    let value_car = document.getElementById("input_car").value;
+    let value_number = document.getElementById("input_numb").value;
+
+    if (value_name == "") {
+        alert("A név nincs kitöltve");
+    } else if (value_car == "") {
+        alert("A kocsi típus nincs kitöltve");
+    } else if (value_number == "" || value_number < 0) {
+        alert("Rossz kilométer van megadva");
+    }
+
+    if(value_name && value_car && value_number) {
+        alert("Mindegyik sikeresen kitöltve");
+    }
+}
